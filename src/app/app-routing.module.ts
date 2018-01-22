111e37f1dff9a96e599d0f02be99b2df8ca28eaa
Passakorn Rattanaprapan <passakorn_working@hotmail.com>
2018-01-22 22:00:09 +0700

    Add auth page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,10 @@ import { Routes, RouterModule } from '@angular/router'
 
 const routes: Routes = [
   {
+    path: 'auth',
+    loadChildren: './auth/auth.module#AuthModule'
+  },
+  {
     path: ':project-id',
     loadChildren: './project/project.module#ProjectModule'
   }
