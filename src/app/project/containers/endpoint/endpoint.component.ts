704fd7f7ce11fe50a35b33dd851bf7b325f72584
Passakorn Rattanaprapan <passakorn_working@hotmail.com>
2018-01-25 11:37:29 +0700

    Update endpoint event path

--- src/app/project/containers/endpoint/endpoint.component.ts
+++ src/app/project/containers/endpoint/endpoint.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store'
+import * as endpointAction from '../../actions/endpoint.action'
+import * as fromProject from '../../reducers'
 
 @Component({
   selector: 'app-endpoint',
@@ -10,12 +13,19 @@ export class EndpointComponent implements OnInit {
   public name: string
   public path: string
 
-  constructor () {
+  constructor (
+    private store: Store<any>
+  ) {
     this.name = 'Check Promotion'
     this.path = '/check-promotion/{user-id}'
   }
 
   ngOnInit () {
+    this.store.dispatch(new endpointAction.PathAction(this.path))
+    this.store.select(fromProject.getEndpointPath).subscribe(a => console.log(a))
   }
 
+  onPathChange (path) {
+    this.store.dispatch(new endpointAction.PathAction(path))
+  }
 }
