afc7cc0e110a009a303c364afe1ecb0ee26ae08c
darklink <passakorn_working@hotmail.com>
2018-01-29 00:50:24 +0700

    Remove code not use

--- src/app/project/containers/config/config.component.ts
+++ src/app/project/containers/config/config.component.ts
@@ -6,58 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./config.component.scss']
 })
 export class ConfigComponent implements OnInit {
-  public path: string
-  
-  public param: any
-  public tempParam: any
 
-  public env: any
-  public tempEnv: any
-
-  constructor() { }
+  constructor () { }
 
   ngOnInit () {
-    this.tempParam = {}
-    this.tempEnv = {}
-  }
-
-  onPathChange (path: string) {
-    this.paramsFilter(path)
-    this.envsFilter(path) 
-  }
-
-  paramsFilter (path) {
-    this.param = {}
-    const paramsRegExp = /\{\$param\.([A-Za-z0-9|\.]*)\}/g
-    const keys = this.keysFilter(paramsRegExp, path)
-    for (const key of keys) {
-      this.param[key] = this.tempParam[key] || ''
-    }
-  }
-
-  envsFilter (path) {
-    this.env = {}
-    const envsRegExp = /\{\$env\.([A-Za-z0-9|\.]+)\}/g
-    const keys = this.keysFilter(envsRegExp, path)
-    for (const key of keys) {
-      this.env[key] = this.tempEnv[key] || ''
-    }
-  }
-
-  keysFilter (pattern, path) {
-    const match = path.match(pattern) || []
-    const keys = match
-      .map(param => new RegExp(pattern).exec(param).slice(1).pop())
-      .filter(param => param !== '' && !new RegExp(/\.{2,}|\.$/g).test(param))
-    return keys
-  }
-
-  onParamValueChange (key: string, value: string) {
-    this.tempParam[key] = value
-  }
-
-  onEnvValueChange (key: string, value: string) {
-    this.tempEnv[key] = value
   }
 
 }
