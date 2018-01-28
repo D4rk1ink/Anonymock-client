f4adca10a13f902ae2afdee14b49c84b538600c4
darklink <passakorn_working@hotmail.com>
2018-01-29 00:49:38 +0700

    Apply main-layout

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common'
 import { RouterModule } from '@angular/router'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { AceEditorModule } from 'ng2-ace-editor'
+
+import { MainLayoutComponent } from './components/main-layout/main-layout.component'
 import { LeftMenuComponent } from './components/left-menu/left-menu.component';
 import { SearchInputComponent } from './components/search-input/search-input.component';
-import { BoxTabsComponent } from './components/box-tabs/box-tabs.component'
+import { BoxTabsComponent } from './components/box-tabs/box-tabs.component';
 
 @NgModule({
   imports: [
@@ -16,6 +18,7 @@ import { BoxTabsComponent } from './components/box-tabs/box-tabs.component'
     AceEditorModule
   ],
   declarations: [
+    MainLayoutComponent,
     LeftMenuComponent,
     SearchInputComponent,
     BoxTabsComponent,
@@ -27,6 +30,7 @@ import { BoxTabsComponent } from './components/box-tabs/box-tabs.component'
     ReactiveFormsModule,
     AceEditorModule,
 
+    MainLayoutComponent,
     LeftMenuComponent,
     SearchInputComponent,
     BoxTabsComponent,
