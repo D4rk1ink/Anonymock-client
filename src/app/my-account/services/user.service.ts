f3eae3a10af67058eb41653189dc54b6adfbf803
Passakorn Rattanaprapan <passakorn_working@hotmail.com>
2018-02-18 20:08:14 +0700

    Update profile my-account

---
+++ src/app/my-account/services/user.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { HttpClient } from '@angular/common/http'
+import * as constants from 'app/shared/constants'
+
+@Injectable()
+export class UserService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getById (id): Observable<any> {
+    const url = constants.BASE_API + '/user/' + id
+    return this.http.get(url)
+  }
+
+  update (id, payload): Observable<any> {
+    const url = constants.BASE_API + '/user/' + id
+    return this.http.patch(url, payload)
+  }
+
+  changePassword (id, payload): Observable<any> {
+    const url = constants.BASE_API + '/user/' + id + '/change-password'
+    return this.http.patch(url, payload)
+  }
+
+}
