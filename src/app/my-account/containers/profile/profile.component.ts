d62ae7565133a5ec339e536dd02e5c942adf582a
Passakorn Rattanaprapan <passakorn_working@hotmail.com>
2018-02-18 20:57:17 +0700

    Update profile my-account

--- src/app/my-account/containers/profile/profile.component.ts
+++ src/app/my-account/containers/profile/profile.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'app/my-account/services/user.service';
+import { Store } from '@ngrx/store';
 import * as database from 'app/core/services/database.service'
+import * as userAction from 'app/core/actions/user.action'
 
 @Component({
   selector: 'profile',
@@ -12,16 +14,35 @@ export class ProfileComponent implements OnInit {
   public user: any
 
   constructor(
+    private store: Store<any>,
     private userService: UserService
   ) {
-    const user = database.getUser()
-    this.userService.getById(user.id)
-      .subscribe(user => {
-        this.user = user
+    this.user = database.getUser()
+    this.userService.getById(this.user.id)
+      .subscribe(res => {
+        if (!res.error) {
+          this.user = res.data
+        }
       })
   }
 
   ngOnInit() {
   }
 
+  onSubmit () {
+    const payload = {
+      firstname: this.user.firstname,
+      lastname: this.user.lastname,
+      email: this.user.email,
+    }
+    this.userService.update(this.user.id, payload)
+      .subscribe(res => {
+        if (!res.error) {
+          this.store.dispatch(new userAction.FirstnameAction(res.data.firstname))
+          this.store.dispatch(new userAction.LastnameAction(res.data.lastname))
+          this.store.dispatch(new userAction.EmailAction(res.data.email))
+        } else {
+        }
+      })
+  }
 }
