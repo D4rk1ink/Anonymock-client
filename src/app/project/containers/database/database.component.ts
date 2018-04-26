5773a87f1d6d30299109c84889ca186050ee9479
Passakorn Rattanaprapan <passakorn_working@hotmail.com>
2018-04-26 12:32:34 +0700

    Apply export database

--- src/app/project/containers/database/database.component.ts
+++ src/app/project/containers/database/database.component.ts
@@ -25,14 +25,14 @@ export class DatabaseComponent implements OnInit {
         this.databaseService.get({ project: id })
           .subscribe(res => {
             if (!res.error) {
-              const data = json.pretty(res.data.data)
-              const schema = json.pretty(res.data.schema)
-              const generate = json.pretty(res.data.generate)
+              const data = res.data.data
+              const schema = res.data.schema
+              const generate = res.data.generate
               const database = {
                 data: data,
                 schema: schema,
                 generate: generate,
-                custom: '[]'
+                custom: []
               }
               this.store.dispatch(new databaseAction.DatabaseAction(database))
             }
