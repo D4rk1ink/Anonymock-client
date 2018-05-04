86edcd7210edab90693a18e1a4c0311a62786c15
Passakorn Rattanaprapan <passakorn_working@hotmail.com>
2018-05-04 12:40:42 +0700

    Convert database response to string

--- src/app/project/containers/database/database.component.ts
+++ src/app/project/containers/database/database.component.ts
@@ -25,9 +25,9 @@ export class DatabaseComponent implements OnInit {
         this.databaseService.get({ project: id })
           .subscribe(res => {
             if (!res.error) {
-              const data = res.data.data
-              const schema = res.data.schema
-              const generate = res.data.generate
+              const data = JSON.stringify(res.data.data)
+              const schema = JSON.stringify(res.data.schema)
+              const generate = JSON.stringify(res.data.generate)
               const database = {
                 data: data,
                 schema: schema,
